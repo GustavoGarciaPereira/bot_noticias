178d1dd43b7d9d236e37ac17321841369efbdc36
gustavo <gusgurtavo@gmail.com>
2023-12-19 10:11:39 -0300

    add dockerfile

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Inicia o bot do Telegram em background
-python /app/telegram/bot.py &
+python /app/bot_telegram/telegram_server.py &
 
 # Inicia o aplicativo FastAPI
 uvicorn fast_api.main:app --host 0.0.0.0 --port 8080
